a355dcc1ca6ce132206b4e0097ea3dfe062ba970
Javier Reyes <javier.reyes@adidas.com>
2022-03-10 21:05:37 +0100

    Small syntax change

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
     });
   };
 
-  const deleteItemHandler = goalId => {
+  const deleteItemHandler = (goalId) => {
     setCourseGoals(prevGoals => {
       const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
       return updatedGoals;
